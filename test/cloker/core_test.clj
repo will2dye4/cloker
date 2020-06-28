4140e44d428ddda1a5cf3950c6e32b78ff77c647
William Dye <will2dye4@mac.com>
2020-06-28 00:30:13 -0400

    First unit test

--- test/cloker/core_test.clj
+++ test/cloker/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [cloker.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest c-function
+  (testing "Creating a card from a string"
+    (testing "succeeds with valid input"
+      (doseq [rank ["2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K" "A"]
+              suit ["C" "D" "H" "S"]]
+        (is (not (nil? (c (str rank suit)))))))
+    (testing "fails with invalid input"
+      (doseq [rank ["C" "0" "1" "11"]
+              suit ["A" "X" "?"]]
+        (is (nil? (c (str rank suit))))))
+    (testing "throws exception if preconditions are not met"
+      (doseq [value ["" "A" "S" "ASAS" :not-a-string 1 {:rank "A" :suit "S"}]]
+        (is (thrown? AssertionError (c value)))))))
