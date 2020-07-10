9c36c6f224bd5aac9c5a8d364de1f26daea20bdc
William Dye <will2dye4@mac.com>
2020-07-09 20:22:01 -0400

    Fix hand potential evaluation

--- src/cloker/ai/cloki.clj
+++ src/cloker/ai/cloki.clj
@@ -2,7 +2,8 @@
   (:require [clojure.math.combinatorics :refer [combinations]]
             [cloker.cards :refer [new-deck]]
             [cloker.constants :refer [board-size num-hole-cards]]
-            [cloker.rating :refer [excluding rate-hand]]))
+            [cloker.rating :refer [excluding rate-hand]]
+            [cloker.utils :refer [map-vals sum]]))
 
 ;; Hand strength calculations based on the "Loki" agent and algorithms described in
 ;; "Opponent Modeling in Poker" (Billings, Papp, Schaeffer, & Szafron, 1998).
@@ -23,10 +24,12 @@
     = :tied
     > :ahead))
 
+(def ^:private initial-hand-strengths {:ahead 0 :tied 0 :behind 0})
+
 (defn hand-strength [hand board]
   (let [cards (concat hand board)
         hand-rating (rate-hand cards)]
-    (loop [hs {:ahead 0 :tied 0 :behind 0} all-opponent-hands (all-possible-opponent-hands cards)]
+    (loop [hs initial-hand-strengths all-opponent-hands (all-possible-opponent-hands cards)]
       (if-let [opponent-hand (first all-opponent-hands)]
         (let [opponent-rating (rate-hand (concat opponent-hand board))
               key (relative-value hand-rating opponent-rating)]
@@ -37,35 +40,25 @@
 (defn adjusted-hand-strength [hand board num-players]
   (* (Math/pow (hand-strength hand board) (dec num-players)) 100))
 
-(def ^:private initial-sub-potentials {:ahead 0 :tied 0 :behind 0})
+(def ^:private initial-hand-potentials (map-vals (constantly initial-hand-strengths) initial-hand-strengths))
 
-(def ^:private initial-hand-potentials {:ahead initial-sub-potentials
-                                        :tied initial-sub-potentials
-                                        :behind initial-sub-potentials})
-
-;; TODO - fix this
 (defn hand-potential [hand board]
   (let [cards (concat hand board)
         hand-rating (rate-hand cards)]
     (loop [hp initial-hand-potentials
-           hp-total initial-sub-potentials
            all-opponent-hands (all-possible-opponent-hands cards)]
       (if-let [opponent-hand (first all-opponent-hands)]
         (let [opponent-rating (rate-hand (concat opponent-hand board))
               key (relative-value hand-rating opponent-rating)
               num-remaining-cards (- board-size (count board))
               all-remaining-cards (all-possible-remaining-cards (concat cards opponent-hand) num-remaining-cards)
-              sp (loop [sp (hp key)
-                        all-remaining-cards all-remaining-cards]
-                   (if-let [remaining-cards (first all-remaining-cards)]
-                     (let [board (concat board remaining-cards)
-                           hand-rating (rate-hand (concat hand board))
-                           opponent-rating (rate-hand (concat opponent-hand board))
-                           key (relative-value hand-rating opponent-rating)]
-                       (recur (update sp key inc) (rest all-remaining-cards)))
-                     sp))]
-          (recur (assoc hp key sp) (update hp-total key inc) (rest all-opponent-hands)))
-        (let [hp-of (fn [k1 k2] (get-in hp [k1 k2]))
+              hs (->> all-remaining-cards
+                      (pmap #(relative-value (rate-hand (concat hand board %)) (rate-hand (concat opponent-hand board %))))
+                      frequencies
+                      (merge-with + (hp key)))]
+          (recur (assoc hp key hs) (rest all-opponent-hands)))
+        (let [hp-total (map-vals #(sum (map val %)) hp)
+              hp-of (fn [k1 k2] (get-in hp [k1 k2]))
               p-pot (/ (+ (hp-of :behind :ahead)
                           (/ (hp-of :behind :tied) 2)
                           (/ (hp-of :tied :ahead) 2))
